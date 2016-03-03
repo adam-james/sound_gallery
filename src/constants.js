bf5af5057c9d78123099b95f4cc22d357627fcbe
adam-james <adam.j.sulewski@gmail.com>
2016-03-02 21:38:24 -0800

    remove europeana consts

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,7 @@
-export const EUROPEANA_API_ROOT = 'http://europeana.eu/api/v2/search.json';
 export const FLICKR_BASE_URL = 'http://api.flickr.com/services/feeds/photos_public.gne';
 
 export const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
 export const CLEAR_SEARCH_QUERY = 'CLEAR_SEARCH_QUERY';
-
-export const EUROPEANA_API_REQUEST = 'EUROPEANA_API_REQUEST';
-export const EUROPEANA_API_SUCCESS = 'EUROPEANA_API_SUCCESS';
-export const EUROPEANA_API_FAILURE = 'EUROPEANA_API_FAILURE';
 
 export const FLICKR_API_REQUEST = 'FLICKR_API_REQUEST';
 export const FLICKR_API_SUCCESS = 'FLICKR_API_SUCCESS';
